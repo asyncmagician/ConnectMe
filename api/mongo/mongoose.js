eb06e4d0ea37b28865e464b35a0d1be386cb1243
PROJKT <giammarino.antony@gmail.com>
2023-07-06 19:54:18 +0200

    :lock: Fix security issues with database credentials

--- api/mongo/mongoose.js
+++ api/mongo/mongoose.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
-
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/api", {
-            pass: "api_PASS",
-            user: "api_USER",
-        })
+        await mongoose.connect(`mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+            pass: process.env.DB_PASS,
+            user: process.env.DB_USER,
+        });
         console.log('You are successfully connected to the database');
     } catch (err) {
         console.error('Erreur while connecting to the database', err);
         process.exit(1);
     }
-}
+};
 
 module.exports = connectDB;
